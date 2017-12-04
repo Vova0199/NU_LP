e75a24c3dd041dd04edd13b452752c7d338efa9a
Vova0199 <vova0143@gmail.com>
2017-12-04 09:00:20 +0200

    Added 5 lab algorithms

---
+++ 2_course/algorithms/word_ladder/lab_5.py
@@ -0,0 +1,27 @@
+class lab_5(object):
+    def ladderLength(self, beginWord, endWord, wordList):
+        distance = 0
+        cur = [beginWord]
+        visited = [beginWord]
+        lookup = set(wordList)
+
+        while cur:
+            next_queue = []
+
+            for word in cur:
+                if word == endWord:
+                    return distance + 1
+                for i in range(len(word)):
+                    for j in 'abcdefghijklmnopqrstuvwxyz':
+                        candidate = word[:i] + j + word[i + 1:]
+                        if candidate not in visited and candidate in lookup:
+                            next_queue.append(candidate)
+                            visited.append(candidate)
+            distance += 1
+            cur = next_queue
+        return 0
+
+
+if __name__ == "__main__":
+  #  print(Solution().ladderLength("hit", "cog", {"hot", "dot", "dog", "lot", "log"}))
+    print(lab_5().ladderLength("hit", "cog", {"hot", "dot", "dog", "lot", "log", "cog"}))
