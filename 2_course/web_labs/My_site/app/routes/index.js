8c61c309a4600db2a462ec902429b377e2c64df6
Vova0199 <vova0143@gmail.com>
2017-12-06 21:24:39 +0200

    [^] Finish 11 lab WEB

--- 2_course/web_labs/My_site/app/routes/index.js
+++ 2_course/web_labs/My_site/app/routes/index.js
@@ -1,15 +1,4 @@
 const noteRoutes = require('./note_routes');
 module.exports = function(app, db) {
-  app.post('/notes', (req, res) => {
-    console.log(req.body)
-  
-      const note = { text: req.body.body, title: req.body.title };
-      db.collection('notes').insert(note, (err, result) => {
-        if (err) {
-          res.send({ 'error': 'An error has occurred' });
-        } else {
-          res.send(result.ops[0]);
-        }
-      });
-  });
+    noteRoutes(app, db);
 };
