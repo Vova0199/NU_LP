03cb60b979433dc5be5dc5362d642283a3f2e766
Vova0199 <vova0143@gmail.com>
2018-09-05 21:16:06 +0300

    [+] Add 2nd lab

--- 3_course/1_semester/labs/app/src/main/java/com/nulp/vp/labs/MainActivity.java
+++ 3_course/1_semester/labs/app/src/main/java/com/nulp/vp/labs/MainActivity.java
@@ -3,16 +3,52 @@ package com.nulp.vp.labs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tv_name;
+    private EditText et_name;
+    private Button btn_set_name, btn_clear;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Hello world!!!");
-        Log.i("Hello world", "It's work");
+        init();
 
+        btn_set_name.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        name = et_name.getText().toString();
+                        if (et_name.getText().toString().trim().equals("")) {
+                            Toast.makeText(getApplicationContext(), "Enter your name", Toast.LENGTH_LONG).show();
+                        } else {
+                            tv_name.setText("Hello " + name);
+                            et_name.setText("");
+                        }
+                    }
+                });
+
+        btn_clear.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        et_name.setText("");
+                    }
+                });
+    }
+
+    private void init() {
+        tv_name = findViewById(R.id.tv_name);
+        btn_set_name = findViewById(R.id.btn_set_name);
+        btn_clear = findViewById(R.id.btn_clear);
+        et_name = findViewById(R.id.et_name);
     }
 }
